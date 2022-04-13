f2867080ecdbea0ded571f6b24fef41f3890081f
ujjawal <ujjawals229@gmail.com>
2022-04-13 20:10:06 +0530

    section-one done

--- script.js
+++ script.js
@@ -4,11 +4,6 @@ console.log("hey");
 let menu = document.querySelector("#menu");
 let navlist = document.querySelector(".nav_list");
 
-/*menu.addEventListener("click", function () {
-  console.log("click");
-  menu.classList.toggle("fa-times");
-  navlist.classList.toggle("active");
-});*/
 
 menu.onclick = () => {
   menu.classList.toggle("fa-times");
@@ -18,3 +13,18 @@ window.onscroll = () => {
   menu.classList.remove("fa-times");
   navlist.classList.remove("active");
 };
+
+let slides = document.querySelectorAll(".slide-container");
+let index = 0;
+
+function next() {
+  slides[index].classList.remove("active");
+  index = (index + 1)% slides.length;
+  slides[index].classList.add("active");
+}
+
+function prev() {
+  slides[index].classList.remove("active");
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add("active");
+}
