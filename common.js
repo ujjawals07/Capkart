49f28dc0156c7ccc60a6f13757b9ee484af62090
ujjawal <ujjawals229@gmail.com>
2022-04-19 00:22:56 +0530

    cart done i have changed a lot here in cart

---
+++ common.js
@@ -0,0 +1,112 @@
+console.log("hi");
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+let removebtn = document.getElementsByClassName("delet");
+function ready() {
+  for (let i = 0; i < removebtn.length; i++) {
+    let rm = removebtn[i];
+    rm.addEventListener("click", function (event) {
+      removeitem();
+    });
+  }
+  let inputchange = document.querySelectorAll(".cart-quantity");
+  inputchange.forEach(function (i) {
+    i.addEventListener("change", quantitychange);
+  });
+  let addtocartbtn = document.getElementsByClassName("btn-cart");
+  for (let i = 0; i < addtocartbtn.length; i++) {
+    const button = addtocartbtn[i];
+    button.addEventListener("click", chomu);
+  }
+
+  document
+    .getElementsByClassName("btn-buy")[0]
+    .addEventListener("click", buyclicked);
+}
+
+function buyclicked() {
+  alert("thank you  for shopping");
+  let cartcontainer = document.getElementsByClassName("cart-container")[0];
+  while (cartcontainer.hasChildNodes()) {
+    cartcontainer.removeChild(cartcontainer.firstChild);
+  }
+  updatecartprice();
+}
+function removeitem(event) {
+  let buttonclicked = event.target;
+  buttonclicked.parentElement.remove();
+
+  updatecartprice();
+}
+
+function chomu(event) {
+  let button = event.target;
+  let shop = button.parentElement;
+  let title = shop.getElementsByClassName("product")[0].innerText;
+  let price = shop.getElementsByClassName("price")[0].innerText;
+  let productimg = shop.getElementsByClassName("cap-img")[0].src;
+  addtoproduct(title, price, productimg);
+  updatecartprice();
+}
+function addtoproduct(title, price, productimg) {
+  let cartbox = document.createElement("div");
+  cartbox.classList.add("cart-box");
+  //cartbox.innerText = title;
+  let cartcontainerbox = document.getElementsByClassName("cart-container")[0];
+  let productname = cartcontainerbox.getElementsByClassName("cart-item");
+  for (let i = 0; i < productname.length; i++) {
+    if (productname[i].innerText == title) {
+      alert("alredy added");
+      return;
+    }
+  }
+  console.log(productname);
+  let cartinnerhtml = `
+    <img src="${productimg}" alt="" class="cart-pic" />
+    <div class="cart-details">
+      <p class="cart-item">${title}</p>
+      <div class="cart-price">${price}</div>
+      <input type="number" value="1" class="cart-quantity" />
+    </div>
+    <div class="delet">Remove</div>`;
+  cartbox.innerHTML = cartinnerhtml;
+  cartcontainerbox.append(cartbox);
+  cartbox
+    .getElementsByClassName("delet")[0]
+    .addEventListener("click", removeitem);
+  cartbox
+    .getElementsByClassName("cart-quantity")[0]
+    .addEventListener("change", quantitychange);
+}
+
+function quantitychange(event) {
+  inputq = event.target;
+  if (isNaN(inputq.value) || inputq.value <= 0) {
+    inputq.value = 1;
+  }
+  updatecartprice();
+}
+
+function updatecartprice() {
+  let cartcontainer = document.getElementsByClassName("cart-container")[0];
+  let cartbox = cartcontainer.getElementsByClassName("cart-box");
+  let total = 0;
+  for (let i = 0; i < cartbox.length; i++) {
+    let cartboxes = cartbox[i];
+    let cartprice = cartboxes.getElementsByClassName("cart-price")[0];
+
+    let cartvalue = cartboxes.getElementsByClassName("cart-quantity")[0];
+
+    let price = parseFloat(cartprice.innerText.replace("$", ""));
+    let quantity = cartvalue.value;
+    total = total + price * quantity;
+    console.log(total);
+  }
+  total = Math.round(total * 100) / 100;
+  let a = (document.getElementsByClassName("total-price")[0].innerText =
+    "$" + total);
+  console.log(a);
+}
--- script.js
+++ script.js
@@ -3,7 +3,6 @@ console.log("hey");
 
 let menu = document.querySelector("#menu");
 let navlist = document.querySelector(".nav_list");
-
 menu.onclick = () => {
   menu.classList.toggle("fa-times");
   navlist.classList.toggle("active");
@@ -58,3 +57,317 @@ review.forEach(function (r) {
     reviewsection.scrollIntoView({ behavior: "smooth" });
   });
 });
+
+/* <section class="products2" id="products2">
+
+<h4 class="products-heading"> Upcoming <span>Caps</span> </h1>
+      
+<div class="cap-box-container">
+
+    <div class="cap-box">
+        <div class="icon">
+            <a href="#" class="fas fa-heart  ic"></a>
+            <a href="#" class="fas fa-share ic"></a>
+            <a href="#" class="fas fa-eye ic"></a>
+        </div>
+        <img src="img/png1 (1).png" alt="" class="cap-img img2">
+        <div class="content">
+            <h3>Cap sumo</h3>
+            <div class="price">$120.99 <span>$150.99</span></div>
+            <div class="stars">
+                <i class="fas fa-star i3"></i>
+                <i class="fas fa-star i3"></i>
+                <i class="fas fa-star i3"></i>
+                <i class="fas fa-star i3"></i>
+                <i class="far fa-star i3"></i>
+            </div>
+            <a  type="button"  class="btn-cart">add to cart</a>
+        </div>
+    </div>
+
+    <div class="cap-box">
+        <div class="icon">
+            <a href="#" class="fas fa-heart ic"></a>
+            <a href="#" class="fas fa-share ic"></a>
+            <a href="#" class="fas fa-eye ic"></a>
+        </div>
+        <img src="img/png2.png" alt="" class="cap-img img2"> 
+        <div class="content">
+            <h3>Cap roma</h3>
+            <div class="price">$120.99 <span>$150.99</span></div>
+            <div class="stars">
+                <i class="fas fa-star i3"></i>
+                <i class="fas fa-star i3"></i>
+                <i class="fas fa-star i3"></i>
+                <i class="fas fa-star i3"></i>
+                <i class="far fa-star i3"></i>
+            </div>
+            <a  type="button"  class="btn-cart">add to cart</a>
+        </div>
+    </div>
+    <div class="cap-box">
+      <div class="icon">
+          <a href="#" class="fas fa-heart ic"></a>
+          <a href="#" class="fas fa-share ic"></a>
+          <a href="#" class="fas fa-eye ic"></a>
+      </div>
+      <img src="img/png3.png" alt="" class="cap-img img2"> 
+      <div class="content">
+          <h3>Cap Puma</h3>
+          <div class="price">$120.99 <span>$150.99</span></div>
+          <div class="stars">
+              <i class="fas fa-star i3"></i>
+              <i class="fas fa-star i3"></i>
+              <i class="fas fa-star i3"></i>
+              <i class="fas fa-star i3"></i>
+              <i class="far fa-star i3"></i>
+          </div>
+          <a type="button" class="btn-cart">add to cart</a>
+      </div>
+  </div>
+  <div class="cap-box">
+    <div class="icon">
+        <a href="#" class="fas fa-heart ic"></a>
+        <a href="#" class="fas fa-share ic"></a>
+        <a href="#" class="fas fa-eye ic"></a>
+    </div>
+    <img src="img/png4.png" alt="" class="cap-img img2"> 
+    <div class="content">
+        <h3>Cap Monk</h3>
+        <div class="price">$120.99 <span>$150.99</span></div>
+        <div class="stars">
+            <i class="fas fa-star i3"></i>
+            <i class="fas fa-star i3"></i>
+            <i class="fas fa-star i3"></i>
+            <i class="fas fa-star i3"></i>
+            <i class="far fa-star i3"></i>
+        </div>
+        <a  type="button"  class="btn-cart">add to cart</a>
+    </div>
+</div>
+<div class="cap-box">
+  <div class="icon">
+      <a href="#" class="fas fa-heart ic"></a>
+      <a href="#" class="fas fa-share ic"></a>
+      <a href="#" class="fas fa-eye ic"></a>
+  </div>
+  <img src="img/army.png" alt="" class="cap-img img2"> 
+  <div class="content">
+      <h3>Cap army</h3>
+      <div class="price">$120.99 <span>$150.99</span></div>
+      <div class="stars">
+          <i class="fas fa-star i3"></i>
+          <i class="fas fa-star i3"></i>
+          <i class="fas fa-star i3"></i>
+          <i class="fas fa-star i3"></i>
+          <i class="far fa-star i3"></i>
+      </div>
+      <a  type="button"  class="btn-cart">add to cart</a>
+  </div>
+</div>
+<div class="cap-box">
+<div class="icon">
+    <a href="#" class="fas fa-heart ic"></a>
+    <a href="#" class="fas fa-share ic"></a>
+    <a href="#" class="fas fa-eye ic"></a>
+</div>
+<img src="img/png5.png" alt="" class="cap-img img2"> 
+<div class="content">
+    <h3>Cap star</h3>
+    <div class="price">$120.99 <span>$150.99</span></div>
+    <div class="stars">
+        <i class="fas fa-star i3"></i>
+        <i class="fas fa-star i3"></i>
+        <i class="fas fa-star i3"></i>
+        <i class="fas fa-star i3"></i>
+        <i class="far fa-star i3"></i>
+    </div>
+    <a  type="button"  class="btn-cart">add to cart</a>
+</div>
+</div>
+  </div>
+  </section>
+  
+  
+         <div class="cap-box">
+              <div class="icon">
+                  <a href="#" class="fas fa-heart  ic"></a>
+                  <a href="#" class="fas fa-share ic"></a>
+                  <a href="#" class="fas fa-eye ic"></a>
+              </div>
+              <img src="img/cap6.jpg" alt="" class="cap-img">
+              <div class="content">
+                  <h3>Cap cobra</h3>
+                  <div class="price">$120.99 <span>$150.99</span></div>
+                  <div class="stars">
+                      <i class="fas fa-star i3"></i>
+                      <i class="fas fa-star i3"></i>
+                      <i class="fas fa-star i3"></i>
+                      <i class="fas fa-star i3"></i>
+                      <i class="far fa-star i3"></i>
+                  </div>
+                <button type="button" class="btn-cart">Add to Cart</button>
+              </div>
+          </div>
+  
+          <div class="cap-box">
+              <div class="icon">
+                  <a href="#" class="fas fa-heart ic"></a>
+                  <a href="#" class="fas fa-share ic"></a>
+                  <a href="#" class="fas fa-eye ic"></a>
+              </div>
+              <img src="img/cap5.jpg" alt="" class="cap-img"> 
+              <div class="content">
+                  <h3>Cap Adidas</h3>
+                  <div class="price">$120.99 <span>$150.99</span></div>
+                  <div class="stars">
+                      <i class="fas fa-star i3"></i>
+                      <i class="fas fa-star i3"></i>
+                      <i class="fas fa-star i3"></i>
+                      <i class="fas fa-star i3"></i>
+                      <i class="far fa-star i3"></i>
+                  </div>
+                  <button type="button" class="btn-cart">Add to Cart</button>
+              </div>
+          </div>
+          <div class="cap-box">
+            <div class="icon">
+                <a href="#" class="fas fa-heart ic"></a>
+                <a href="#" class="fas fa-share ic"></a>
+                <a href="#" class="fas fa-eye ic"></a>
+            </div>
+            <img src="img/cap4.jpg" alt="" class="cap-img"> 
+            <div class="content">
+                <h3>Cap Puma</h3>
+                <div class="price">$120.99 <span>$150.99</span></div>
+                <div class="stars">
+                    <i class="fas fa-star i3"></i>
+                    <i class="fas fa-star i3"></i>
+                    <i class="fas fa-star i3"></i>
+                    <i class="fas fa-star i3"></i>
+                    <i class="far fa-star i3"></i>
+                </div>
+                <button type="button" class="btn-cart">Add to Cart</button>
+            </div>
+        </div>
+        <div class="cap-box">
+          <div class="icon">
+              <a href="#" class="fas fa-heart ic"></a>
+              <a href="#" class="fas fa-share ic"></a>
+              <a href="#" class="fas fa-eye ic"></a>
+          </div>
+          <img src="img/cap3.jpg" alt="" class="cap-img"> 
+          <div class="content">
+              <h3>Cap Monkey</h3>
+              <div class="price">$120.99 <span>$150.99</span></div>
+              <div class="stars">
+                  <i class="fas fa-star i3"></i>
+                  <i class="fas fa-star i3"></i>
+                  <i class="fas fa-star i3"></i>
+                  <i class="fas fa-star i3"></i>
+                  <i class="far fa-star i3"></i>
+              </div>
+              <button type="button" class="btn-cart">Add to Cart</button>
+          </div>
+      </div>
+      <div class="cap-box">
+        <div class="icon">
+            <a href="#" class="fas fa-heart ic"></a>
+            <a href="#" class="fas fa-share ic"></a>
+            <a href="#" class="fas fa-eye ic"></a>
+        </div>
+        <img src="img/cap2.jpg" alt="" class="cap-img"> 
+        <div class="content">
+            <h3>Cap Calvine</h3>
+            <div class="price">$120.99 <span>$150.99</span></div>
+            <div class="stars">
+                <i class="fas fa-star i3"></i>
+                <i class="fas fa-star i3"></i>
+                <i class="fas fa-star i3"></i>
+                <i class="fas fa-star i3"></i>
+                <i class="far fa-star i3"></i>
+            </div>
+            <button type="button" class="btn-cart">Add to Cart</button>
+        </div>
+    </div>
+    <div class="cap-box">
+      <div class="icon">
+          <a href="#" class="fas fa-heart ic"></a>
+          <a href="#" class="fas fa-share ic"></a>
+          <a href="#" class="fas fa-eye ic"></a>
+      </div>
+      <img src="img/cap1.jpg" alt="" class="cap-img"> 
+      <div class="content">
+          <h3>Cap roadstar</h3>
+          <div class="price">$120.99 <span>$150.99</span></div>
+          <div class="stars">
+              <i class="fas fa-star i3"></i>
+              <i class="fas fa-star i3"></i>
+              <i class="fas fa-star i3"></i>
+              <i class="fas fa-star i3"></i>
+              <i class="far fa-star i3"></i>
+          </div>
+          <a  type="button"  class="btn-cart">add to cart</a>
+      </div>
+  </div>
+  
+     <a id="close-cart" class="btn-home">Back To Home ↩</a>
+    <div class="cart_container">
+      <h8 class="cart-title">Your cart</h8>
+      <div class="cart"> 
+      
+      <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="stylesheet" href="sass\main.css" />
+    <title>The Pizza Show</title>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+      integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <title>Document</title>
+  </head>
+
+  <body class="cart-body">
+    <a id="close-cart" class="btn-home">Back To Home ↩</a>
+        <div class="cart-container">
+          <div class="cart-box">
+            <img src="img/army.png" alt="" class="cart-pic" />
+            <div class="cart-details">
+              <p class="cart-item">cap army</p>
+              <div class="cart-price">$25</div>
+              <input type="number" value="1" class="cart-quantity" />
+            </div>
+            <div class="delet">Remove</div>
+          </div>
+          <div class="cart-box">
+            <img src="img/army.png" alt="" class="cart-pic" />
+            <div class="cart-details">
+              <p class="cart-item">cap army</p>
+              <div class="cart-price">$70</div>
+              <input type="number" value="1" class="cart-quantity" />
+            </div>
+            <div class="delet">Remove</div>
+          </div>
+    
+        <div class="total">
+          <div class="total-title">Total</div>
+          <div class="total-price">$00</div>
+        </div>
+        <button class="btn-buy">Buy now</button>
+      </div>
+    </div>
+    </body>
+
+   
+    <script defer src="close.js"></script>
+<script src="common.js" async></script>
+  </body>
+</html>*/
